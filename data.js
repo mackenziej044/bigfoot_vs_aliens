d8a57d501d8e81a7c9aed5a4bbda07d32a23aaf9
mackenziej044 <mackenziej044@gmail.com>
2024-10-17 19:46:32 -0400

    commit

--- data.js
+++ data.js
@@ -42,41 +42,83 @@ info.onAdd = function() {
 info.addTo(map);
 
 // Load both datasets using Promise.all
+const bigfootpromise = 'bigfoot_map_data.csv'
+const ufopromise = 'ufo_map_data.csv'
+
 Promise.all([
-    d3.csv('bigfoot_map_data.csv'), // Load Bigfoot sightings from CSV
-    d3.csv('ufo_map_data.csv') // Load UFO sightings from CSV
+    d3.csv('bigfoot_map_data_test10.csv'),
+        d3.csv('ufo_map_data_test10.csv')
 ]).then(([bigfootData, ufoData]) => {
+    console.log(bigfootData)
+    console.log(ufoData)
     // Process Bigfoot sightings data
     bigfootData.forEach(row => {
         let marker = L.marker([row.latitude, row.longitude], { icon: bigfootIcon })
-            .bindPopup(`<b>Classification:</b> ${row.classification}<br><b>Date:</b> ${row.date} <br><b>Report Number:</b> ${row.report_number}`)
+            .bindPopup(`<b>Date:</b> ${row.date} <br><b>Classification:</b> ${row.classification} <br><b>Report Number:</b> ${row.report_number}`)
             .addTo(layers.BIGFOOT_SIGHTINGS);
     });
 
     // Process UFO sightings data
-    ufoData.sightings.forEach(sighting => {
+    ufoData.forEach(sighting => {
         let marker = L.marker([sighting.latitude, sighting.longitude], { icon: ufoIcon })
-            .bindPopup(`<b>Location:</b> ${sighting.location}<br><b>Date:</b> ${sighting.date}`)
+            .bindPopup(`<b>Date:</b> ${sighting.date} <br><b>Shape:</b> ${sighting.shape}<br><b>Duration:</b> ${sighting.duration_hours_min}`)
             .addTo(layers.UFO_SIGHTINGS);
     });
 }).catch(error => {
     console.error('Error loading the datasets:', error);
 });
 
-// Bigfoot icon
+// Bigfoot icon (REVISIT)
 var bigfootIcon = L.icon({
     iconUrl: 'bigfoot.png',
-    iconSize: [38, 95], // size of the icon
-    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    iconSize: [10, 10], // size of the icon
+    iconAnchor: [0, 0], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, 0] // point from which the popup should open relative to the iconAnchor
 });
 
 // UFO icon
 var ufoIcon = L.icon({
     iconUrl: 'ufo.png',
-    iconSize: [38, 95], // size of the icon
-    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    iconSize: [15, 15], // size of the icon
+    iconAnchor: [0, 0], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, 0] // point from which the popup should open relative to the iconAnchor
 });
+
+// Promise.all([
+//     d3.csv("bigfoot_map_data.csv"),
+//     d3.csv("ufo_map_data.csv"),
+// ]).then(function(files) { 
+//     console.log(files)
+//     console.log(files[0]),
+//     console.log(files[1])
+//     // files[0] will contain file1.csv
+//     // files[1] will contain file2.csv
+// }).catch(function(err) {
+//     // handle error here
+
+// })
+
+// // conSize
+// Point
+// null
+// Size of the icon image in pixels.
+// iconAnchor
+// Point
+// null
+// The coordinates of the "tip" of the icon (relative to its top left corner). The icon will be aligned so that this point is at the marker's geographical location. Centered by default if size is specified, also can be set in CSS with negative margins.
+// popupAnchor
+// Point
+// [0, 0]
+// The coordinates of the point from which popups will "open", relative to the icon anchor.
+// tooltipAnchor
+// Point
+// [0, 0]
+// The coordinates of the point from which tooltips will "open", relative to the icon anchor.
+// shadowSize
+// Point
+// null
+// Size of the shadow image in pixels.
+// shadowAnchor
+// Point
+// null
+// The coordinates of the "tip" of the shadow (relative to its top left corner) (the same as iconAnchor if not specified).
